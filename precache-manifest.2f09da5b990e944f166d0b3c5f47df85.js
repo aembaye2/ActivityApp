c6cb62104723c1084cc426fe15974b570d0ffd12
Abel Embaye <aembaye@uark.edu>
2024-10-30 13:48:57 -0500

    Updates

---
+++ precache-manifest.2f09da5b990e944f166d0b3c5f47df85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2b45ca73d58829a36f29e0852613048",
+    "url": "/ActivityApp/index.html"
+  },
+  {
+    "revision": "4ac4f717bc5dfcf35bc0",
+    "url": "/ActivityApp/static/css/main.aa7169d5.chunk.css"
+  },
+  {
+    "revision": "00f94c0db7cb37b8fbfb",
+    "url": "/ActivityApp/static/js/2.3f050581.chunk.js"
+  },
+  {
+    "revision": "85c12b8a5f8b5915f994662c7b9ea06d",
+    "url": "/ActivityApp/static/js/2.3f050581.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ac4f717bc5dfcf35bc0",
+    "url": "/ActivityApp/static/js/main.0b8add3b.chunk.js"
+  },
+  {
+    "revision": "225813e7e42ef7538bfd",
+    "url": "/ActivityApp/static/js/runtime-main.bbb235cc.js"
+  }
+]);
