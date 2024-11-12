489676c12c5ea4cc04024290cd4f71487f10d573
Abel Embaye <aembaye@uark.edu>
2024-11-12 10:24:50 -0600

    commited on 11/12/24b

--- src/pages/ActLes10.js
+++ src/pages/ActLes10.js
@@ -4,7 +4,7 @@ import data from "./questions2.json" // Import the JSON file
 
 const ActLes10 = () => {
   const currentDate = new Date()
-  const targetDate = new Date("2024-11-12T11:30:00")
+  const targetDate = new Date("2024-11-12T09:30:00")
   const endDate = new Date("2024-11-12T12:15:00")
 
   const isAvailable = currentDate >= targetDate && currentDate <= endDate
