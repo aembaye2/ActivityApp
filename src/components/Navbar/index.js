475d60c7adaf325f704179dd62fab21bfa3bae72
Abel Embaye <aembaye@uark.edu>
2025-02-27 18:21:06 -0600

    committed on 2/6/2025e

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -5,7 +5,7 @@ import { Nav, NavLink, Bars, NavMenu } from "./NavbarElements"
 
 const Navbar = () => {
   const currentDate = new Date()
-  const hw02DueDate = new Date("2025-02-24T09:00:00") // Set the due date for Homework 2 with hours and minutes
+  const hw02DueDate = new Date("2025-03-04T23:59:00") // Set the due date for Homework 2 with hours and minutes
 
   return (
     <>
@@ -13,13 +13,13 @@ const Navbar = () => {
         <Bars />
         <NavMenu>
           <NavLink to="/Home">Home</NavLink>
-          {/* <NavLink to="/ActCh00">Ch00</NavLink> */}
-          {/* <NavLink to="/ActCh04">Ch4 Activity</NavLink>
-          // <NavLink to="/ActCh05">Ch5 Activity</NavLink>*/}
+          {/* <NavLink to="/ActCh00">Ch00</NavLink>
+          <NavLink to="/ActCh04">Ch4 Activity</NavLink>
+          <NavLink to="/ActCh05">Ch5 Activity</NavLink> */}
 
-          {currentDate <= hw02DueDate && (
+          {/* {currentDate <= hw02DueDate && (
             <NavLink to="/ActCh06">Ch6 Activity</NavLink>
-          )}
+          )} */}
           {/* <NavLink to="/Hw01">Homework 1</NavLink> */}
           {currentDate <= hw02DueDate && (
             <NavLink to="/Hw02">Homework 2</NavLink>
